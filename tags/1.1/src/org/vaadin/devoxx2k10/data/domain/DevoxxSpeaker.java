0bb08c9a397ad0e74d46f371331ab1f6b0880f85
tehapo <tehapo@edf7c4fb-1dab-0550-e7b9-7ebabcc41ba1>
2010-11-05 12:14:07 +0000

    Tagged 1.1 release.

---
+++ tags/1.1/src/org/vaadin/devoxx2k10/data/domain/DevoxxSpeaker.java
@@ -0,0 +1,12 @@
+package org.vaadin.devoxx2k10.data.domain;
+
+public interface DevoxxSpeaker {
+
+    int getId();
+
+    String getName();
+
+    String getImageUri();
+
+    String getBio();
+}
---
+++ tags/1.1/src/org/vaadin/devoxx2k10/ui/calendar/DevoxxCalendarEvent.java
@@ -0,0 +1,73 @@
+package org.vaadin.devoxx2k10.ui.calendar;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.vaadin.devoxx2k10.data.domain.DevoxxPresentation;
+import org.vaadin.devoxx2k10.util.StringUtil;
+
+import com.vaadin.addon.calendar.event.BasicEvent;
+
+public class DevoxxCalendarEvent extends BasicEvent {
+
+    private static final long serialVersionUID = -5372113758406385246L;
+
+    private DevoxxPresentation devoxxEvent;
+    private Set<String> additionalStyles = new HashSet<String>();
+    private String speakers;
+
+    @Override
+    public Date getStart() {
+        return devoxxEvent.getFromTime();
+    }
+
+    @Override
+    public Date getEnd() {
+        return devoxxEvent.getToTime();
+    }
+
+    @Override
+    public String getCaption() {
+        if (speakers == null) {
+            speakers = StringUtil.getSpeakersString(devoxxEvent);
+        }
+        return devoxxEvent.getTitle() + " " + speakers;
+    }
+
+    public DevoxxPresentation getDevoxxEvent() {
+        return devoxxEvent;
+    }
+
+    public void setDevoxxEvent(final DevoxxPresentation devoxxEvent) {
+        this.devoxxEvent = devoxxEvent;
+    }
+
+    /**
+     * Overridden to allow multiple CSS class names.
+     */
+    @Override
+    public String getStyleName() {
+        final  StringBuilder sb = new StringBuilder(100);
+        sb.append(super.getStyleName());
+        for (String styleName : additionalStyles) {
+            sb.append(" ");
+            sb.append(styleName);
+        }
+        return sb.toString();
+    }
+
+    public void addStyleName(final String styleName) {
+        boolean added = additionalStyles.add(styleName);
+        if (added) {
+            fireEventChange();
+        }
+    }
+
+    public void removeStyleName(final String styleName) {
+        boolean removed = additionalStyles.remove(styleName);
+        if (removed) {
+            fireEventChange();
+        }
+    }
+}
