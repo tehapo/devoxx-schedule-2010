6d9743225382a9351fc4b43b31eec248d29720d3
tehapo <tehapo@edf7c4fb-1dab-0550-e7b9-7ebabcc41ba1>
2010-11-10 13:49:20 +0000

    Branch for version with generated debug ids for JMeter testing.

---
+++ branches/debug-ids-for-jmeter/src/org/vaadin/devoxx2k10/ui/calendar/DevoxxEventProvider.java
@@ -0,0 +1,113 @@
+package org.vaadin.devoxx2k10.ui.calendar;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.vaadin.devoxx2k10.DevoxxScheduleApplication;
+import org.vaadin.devoxx2k10.data.RestApiFacade;
+import org.vaadin.devoxx2k10.data.domain.DevoxxPresentation;
+import org.vaadin.devoxx2k10.data.domain.MyScheduleUser;
+
+import com.vaadin.addon.calendar.event.BasicEventProvider;
+import com.vaadin.addon.calendar.event.CalendarEvent;
+
+public class DevoxxEventProvider extends BasicEventProvider {
+
+    private static final long serialVersionUID = -6066313242075569496L;
+
+    private transient final Logger logger = Logger.getLogger(getClass());
+    private boolean eventsLoaded;
+    private DevoxxPresentation selectedEvent;
+
+    private static final long SHORT_EVENT_THRESHOLD_MS = 1000 * 60 * 30;
+
+    @Override
+    public List<CalendarEvent> getEvents(final Date startDate, final Date endDate) {
+        loadEventsFromBackendIfNeeded();
+
+        final List<CalendarEvent> result = super.getEvents(startDate, endDate);
+
+        // Update the selected style name.
+        for (final CalendarEvent event : eventList) {
+            if (event instanceof DevoxxCalendarEvent) {
+                final DevoxxCalendarEvent devoxxCalEvent = ((DevoxxCalendarEvent) event);
+                if (devoxxCalEvent.getDevoxxEvent().equals(selectedEvent)) {
+                    devoxxCalEvent.addStyleName("selected");
+                } else {
+                    devoxxCalEvent.removeStyleName("selected");
+                }
+            }
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Returning " + result.size() + " events for " + startDate + " - " + endDate);
+        }
+
+        return result;
+    }
+
+    public CalendarEvent getEvent(final int id) {
+        loadEventsFromBackendIfNeeded();
+
+        for (final CalendarEvent event : eventList) {
+            if (event instanceof DevoxxCalendarEvent && ((DevoxxCalendarEvent) event).getDevoxxEvent().getId() == id) {
+                return event;
+            }
+        }
+
+        return null;
+    }
+
+    public void refreshAttendingStyles() {
+        final MyScheduleUser user = (MyScheduleUser) DevoxxScheduleApplication.getCurrentInstance().getUser();
+
+        for (final CalendarEvent event : eventList) {
+            if (event instanceof DevoxxCalendarEvent) {
+                final DevoxxCalendarEvent devoxxEvent = (DevoxxCalendarEvent) event;
+                if (user != null && user.hasFavourited(devoxxEvent.getDevoxxEvent())) {
+                    devoxxEvent.addStyleName("attending");
+                } else {
+                    devoxxEvent.removeStyleName("attending");
+                }
+            }
+        }
+    }
+
+    private void loadEventsFromBackendIfNeeded() {
+        if (eventsLoaded) {
+            // already loaded -> do nothing
+            return;
+        }
+
+        final RestApiFacade facade = DevoxxScheduleApplication.getCurrentInstance().getBackendFacade();
+        final List<DevoxxPresentation> schedule = facade.getFullSchedule();
+
+        // wrap data from the model into CalendarEvents for UI
+        for (final DevoxxPresentation event : schedule) {
+            final DevoxxCalendarEvent calEvent = new DevoxxCalendarEvent();
+            calEvent.setStyleName(event.getKind().name().toLowerCase());
+            calEvent.addStyleName("at-" + event.getRoom().toLowerCase().replaceAll(" ", ""));
+            if (isShortEvent(event)) {
+                calEvent.addStyleName("short-event");
+            }
+            calEvent.setDevoxxEvent(event);
+            calEvent.addListener(this);
+            super.addEvent(calEvent);
+        }
+        eventsLoaded = true;
+        refreshAttendingStyles();
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Fetched schedule from backend (total " + schedule.size() + " events).");
+        }
+    }
+
+    private static boolean isShortEvent(final DevoxxPresentation event) {
+        return event.getToTime().getTime() - event.getFromTime().getTime() < SHORT_EVENT_THRESHOLD_MS;
+    }
+
+    public void setSelectedPresentation(final DevoxxPresentation event) {
+        selectedEvent = event;
+    }
+}
---
+++ branches/debug-ids-for-jmeter/src/org/vaadin/devoxx2k10/util/StringUtil.java
@@ -0,0 +1,55 @@
+package org.vaadin.devoxx2k10.util;
+
+import org.vaadin.devoxx2k10.data.domain.DevoxxPresentation;
+import org.vaadin.devoxx2k10.data.domain.DevoxxSpeaker;
+
+public class StringUtil {
+
+    /**
+     * Returns the duration of given {@link DevoxxPresentation} as hours and
+     * minutes (for example "1h 20min") or just in minutes if the duration is
+     * under one hour (for example "35min").
+     * 
+     * @param event
+     * @return
+     */
+    public static String getEventDuration(final DevoxxPresentation event) {
+        String duration;
+        long durationInMinutes = (event.getToTime().getTime() - event
+                .getFromTime().getTime()) / 1000 / 60;
+        if (durationInMinutes >= 60) {
+            final long durationInFullHours = (durationInMinutes / 60);
+            duration = durationInFullHours + "h";
+            if (durationInMinutes % 60 != 0) {
+                duration += " " + (durationInMinutes - 60 * durationInFullHours) + "min";
+            }
+        } else {
+            duration = durationInMinutes + "min";
+        }
+
+        return duration;
+    }
+
+    /**
+     * Returns the speakers of the event as a String in parenthesis separated
+     * with a comma or an empty String if there are no speakers for the event.
+     * 
+     * @param event
+     * @return the speakers of the event as a String in parenthesis separated
+     *         with a comma or an empty String.
+     */
+    public static String getSpeakersString(final DevoxxPresentation event) {
+        final StringBuilder speakers = new StringBuilder(100);
+        if (!event.getSpeakers().isEmpty()) {
+            speakers.append('(');
+            for (final DevoxxSpeaker speaker : event.getSpeakers()) {
+                if (speakers.length() > 1) {
+                    speakers.append(", ");
+                }
+                speakers.append(speaker.getName());
+            }
+            speakers.append(')');
+        }
+        return speakers.toString();
+    }
+}
